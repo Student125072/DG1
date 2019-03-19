265aab872cc0ac0a613dfc4b6196d51318c8a5d6
Kevin <koster.2001@gmail.com>
2019-03-19 23:12:50 +0100

    begin met stylen van de homepage

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 <link rel="stylesheet" href="css/master.css">
 
   <main>
-
+    <div class="main-body">
+      <h1>Homepage</h1>
+      <p>some text some text some textsome textsome textsome textsome textsome text</p>
+    </div>
   </main>
 
 <?php include('footer.php') ?>
