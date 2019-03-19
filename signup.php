ae725892294b7ca26d716044a5ffae31cb71afc4
Kevin <koster.2001@gmail.com>
2019-03-19 18:23:37 +0100

    registratie form gemaakt, nog niet werken

---
+++ signup.php
@@ -0,0 +1,16 @@
+<?php include 'header.php' ?>
+
+  <link rel="stylesheet" href="css/signup.css">
+  <link rel="stylesheet" href="css/font.css">
+
+    <main class="main-body">
+      <div class="register-form">
+        <h1>Registration</h1>
+        <form align="center" action="includes/signup.inc.php" method="post">
+          <input type="text" name="userID" placeholder="Username">
+          <input type="password" name="pwd" placeholder="Password">
+          <input type="password" name="pwd-check" placeholder="Confirm Password">
+          <button type="submit" name="signup-submit">SIGNUP</button>
+        </form>
+      </div>
+    </main>
