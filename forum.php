6d8f9b2e40301f4ce70038f37fa839537de29553
Kevin <koster.2001@gmail.com>
2019-03-21 10:52:53 +0100

    kleine aanpassingen in het forum

---
+++ forum.php
@@ -0,0 +1,14 @@
+<?php include('header.php') ?>
+
+<link rel="stylesheet" href="css/forum.css">
+
+  <div class="main-body">
+    <h1>Topics</h1>
+    <ul>
+      <li><a href="">HTML</a></li>
+      <li><a href="">CSS</a></li>
+      <li><a href="">PHP</a></li>
+    </ul>
+  </div>
+
+<?php include('footer.php') ?>
